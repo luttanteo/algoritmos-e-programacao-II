0ac5acf5da3f96ef455b520e177d8c0ecde89863
Ana Luiza Alves de Lima <analuizalvesdelima@gmail.com>
2025-03-06 09:29:55 -0300

    Ex2Aula2.java

--- Ex2Aula2.java
+++ Ex2Aula2.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class exercicio2 {
+public class Ex2Aula2 {
     public static void main(String[] args) {
         Random rand = new Random();
         Scanner sc = new Scanner(System.in);
