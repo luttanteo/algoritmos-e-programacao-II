e7a4eea48bfdbdb3494f22506bea7f4363c2b380
Ana Luiza Alves de Lima <analuizalvesdelima@gmail.com>
2025-03-06 09:39:58 -0300

    array4EX.java

--- array4EX.java
+++ array4EX.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class Ex1Aula1 {
+public class array4EX {
 	public static void main(String[] args) {
 		int[][][][] matriz = new int[5][5][5][5];
 		preencher(matriz);
