43d62584d3b6233d29c29dd0751defc036cab099
Ana Luiza Alves de Lima <analuizalvesdelima@gmail.com>
2025-03-06 10:35:29 -0300

    PreencherEX.java

--- PreencherEX.java
+++ PreencherEX.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class Ex2Aula1 {
+public class PreencherEX {
 	public static void main(String[] args) {
 		int[][][] matriz = new int[5][5][5];
 		preencher(matriz);
