425c75a6421ab78340ac450f864d1799cb08ba82
Ana Luiza Alves de Lima <analuizalvesdelima@gmail.com>
2025-03-06 10:47:14 -0300

    palindromoEX.java

---
+++ palindromoEX.java
@@ -0,0 +1,14 @@
+public class palindromoEX {
+    public static void main(String[] args) {
+        System.out.println(ehPalindromo("amor e roma"));
+    }
+    public static boolean ehPalindromo(String palavra) {
+        if (palavra.length() <= 1) {
+            return true;
+        }
+        if (palavra.charAt(0) != palavra.charAt(palavra.length() - 1)) {
+            return false;
+        }
+        return ehPalindromo(palavra.substring(1, palavra.length() - 1));
+    }
+}
