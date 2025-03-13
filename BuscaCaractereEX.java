0cd1b16827ed8f33a7f4f6615bb4f94e0a5960d8
Ana Luiza Alves de Lima <analuizalvesdelima@gmail.com>
2025-03-13 11:21:36 -0300

    BuscaCaractereEX.java

--- BuscaCaractereEX.java
+++ BuscaCaractereEX.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class buscaCaractereEX {
+public class BuscaCaractereEX {
     public static void main(String[] ars) {
         char[] array = new char[1000];
         preencher(array);
