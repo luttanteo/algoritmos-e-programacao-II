b6c4dcd61ff218359e60188512143156b0e169fe
Ana Luiza Alves de Lima <analuizalvesdelima@gmail.com>
2025-03-13 11:21:57 -0300

    SomarEX.java

--- SomarEX.java
+++ SomarEX.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class somarEX {
+public class SomarEX {
     /* Crie um método recursivo que receba como parâmetro um número e retorne a soma
     de todos os números naturais de 1 até este número */
     public static void main(String[] args) {
